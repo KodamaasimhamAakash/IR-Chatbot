293b0cc7be84e1e0a8839c7e04dde01473ee4baf
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-15 00:57:41 +0530

    Final Base Window

--- GUI/Trial/trialBasicStructure.py
+++ GUI/Trial/trialBasicStructure.py
@@ -6,12 +6,21 @@ class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.title = 'Base Structure'
+        self.statusMessage = 'On Github.com'
+        self.top = 300
+        self.left = 300
+        self.height = 300
+        self.width = 500
         self.initUI()
 
     def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top,self.left,self.width,self.height)
+        self.statusBar().showMessage(self.statusMessage)
         self.show()
 
-    if __init__='__main__':
-        app = QApplication(sys.argv)
-        ex = App()
-        sys.exit(app.exec_())
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
