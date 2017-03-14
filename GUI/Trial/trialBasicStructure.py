2bd0368e1edf18ea1759c0645b4f68948b7026c4
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-15 00:45:33 +0530

    WIP Basic Structure

--- GUI/Trial/trialBasicStructure.py
+++ GUI/Trial/trialBasicStructure.py
@@ -1,3 +1,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
 from PyQt5.QtGui import QIcon
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.show()
+
+    if __init__='__main__':
+        app = QApplication(sys.argv)
+        ex = App()
+        sys.exit(app.exec_())
