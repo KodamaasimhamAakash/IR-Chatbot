8db7ede14e9e73489a5d121df69291763c709473
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-15 00:02:21 +0530

    WIP TrialWindow

--- GUI/Trial/guiTrial.py
+++ GUI/Trial/guiTrial.py
@@ -1,3 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
-from PyQt5.QtGui import QIcon
+# from PyQt5.QtGui import QIcon
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(300,300)
+    w.move(100,300)
+    w.setWindowTitle('Trial')
+    w.show()
+
+    sys.exit(app.exec_())
