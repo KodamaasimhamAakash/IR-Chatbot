dd31f18792b6a2b7c11b10a64be92dac6e4707ef
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-19 17:07:26 +0530

    Final MessageBox

--- GUI/Trial/MessageBox.py
+++ GUI/Trial/MessageBox.py
@@ -2,3 +2,21 @@ import sys
 from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QMessageBox
 from PyQt5.QtGui import QIcon
 from PyQT5.QtCore import pyqtSlot
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        answer = QMessageBox.question(self,'Map','Do you want to add the Link?',QMessageBox.Yes|QMessageBox.No, QMessageBox.Yes)
+        if answer == QMessageBox.Yes:
+            print("Yes")
+        if answer == QMessageBox.No
+            print("No")
+        #self.show()
+
+ if __name__=='__main__':
+     app = QApplication(sys.argv)
+     ex = App()
+     sys.exit(app.exec_())
