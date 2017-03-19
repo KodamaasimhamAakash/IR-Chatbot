385f923bb1c9f6abdaa5702c51260ee3397abc73
ShakthiSampath <shakthivvbhss@gmail.com>
2017-03-19 16:29:59 +0530

    Import Statements

--- GUI/Trial/MessageBox.py
+++ GUI/Trial/MessageBox.py
@@ -1,3 +1,4 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QMessageBox
 from PyQt5.QtGui import QIcon
+from PyQT5.QtCore import pyqtSlot
