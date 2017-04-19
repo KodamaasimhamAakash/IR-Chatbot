e1945a0a3fa7805100777bf6381c085fa7001dd9
ShakthiSampath <shakthivvbhss@gmail.com>
2017-04-20 02:59:32 +0530

    Update File

--- GUI/Trial/CreateAcc.py
+++ GUI/Trial/CreateAcc.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow,QLabel
+from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow,QLabel,QLineEdit,QPushButton
 from PyQt5.QtGui import QIcon,QPixmap
 from PyQt5.QtCore import Qt
 
@@ -20,14 +20,31 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left,self.top,self.width,self.height)
         self.statusBar().showMessage(self.statusMessage)
+
+        # declarations
         welcome = QLabel(self)
         botface = QLabel(self)
+        UserNameLabel = QLabel(self)
+        UserNameTextBox = QLineEdit(self)
+        PasswordLabel = QLabel(self)
+        PasswordTextBox = QLineEdit(self)
+        confirmButton = QPushButton(self)
+
+        # set them all
         botlogo = QPixmap('bot.png')
         welcome.setText('Please Register!')
         welcome.setAlignment(Qt.AlignLeft);
         botface.setPixmap(botlogo)
+        confirmButton.setText("Confirm")
+
+        # moving to appropriate positions
+        UserNameLabel.move(10,100)
+        UserNameTextBox.move(150,80)
+        PasswordLabel.move(80,100)
+        PasswordTextBox.move(150,100)
         welcome.move(200,40)
         botface.move(130,35)
+        confirmButton.move(200,200)
 
         self.show()
 
