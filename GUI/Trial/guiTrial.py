a2351cf0d1e87e54fa2abc787711553fd089f4a2
ShakthiSampath <shakthivvbhss@gmail.com>
2017-04-19 19:54:08 +0530

    Added icon and made changes to the previous code

--- GUI/Trial/guiTrial.py
+++ GUI/Trial/guiTrial.py
@@ -1,14 +1,26 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
-# from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QVBoxLayout,QPushButton,QMainWindow
+from PyQt5.QtGui import QIcon
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'ChatBot'
+        self.statusMessage = 'Type your Message...'
+        self.top = 300
+        self.left = 1400
+        self.height = 700
+        self.width = 500
+        self.initUI()  # next method call
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left,self.top,self.width,self.height)
+        self.statusBar().showMessage(self.statusMessage)
+        self.show()
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(300,300)
-    w.move(500,300)
-    w.setWindowTitle('Trial')
-    w.show()
-
+    ex = App() #calls App class
     sys.exit(app.exec_())
