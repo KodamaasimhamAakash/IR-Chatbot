dd022b6a7f428ec6d0b71a1dc91c99f358fa0634
ShakthiSampath <shakthivvbhss@gmail.com>
2017-04-19 22:44:29 +0530

    File Names Changed

--- GUI/Trial/CreateAcc.py
+++ GUI/Trial/CreateAcc.py
@@ -1,31 +1,27 @@
 import sys
 import os
-from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QVBoxLayout,QPushButton,QMainWindow
+from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
 from PyQt5.QtGui import QIcon
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'ChatBot'
-        self.statusMessage = 'Type your Message...'
-        self.top = 300
-        self.left = 1400
-        self.height = 700
+        self.title = 'Create Account'
+        self.statusMessage = 'Create Account...'
+        self.top = 200
+        self.left = 550
+        self.height = 300
         self.width = 500
         self.initUI()  # next method call
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left,self.top,self.width,self.height)
-
-
         self.statusBar().showMessage(self.statusMessage)
         self.show()
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
-    path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'ICON.png')
-    app.setWindowIcon(QIcon(path))
     ex = App() #calls App class
     sys.exit(app.exec_())
--- GUI/Trial/GUI.py
+++ GUI/Trial/GUI.py
@@ -1,26 +1,32 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
+import os
+from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QVBoxLayout,QPushButton,QMainWindow
 from PyQt5.QtGui import QIcon
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'Base Structure'
-        self.statusMessage = 'On Github.com'
-        self.top = 300
-        self.left = 300
-        self.height = 300
+        self.title = 'ChatBot'
+        self.statusMessage = 'Type Your Message...'
+        self.top = 200
+        self.left = 550
+        self.height = 700
         self.width = 500
         self.initUI()  # next method call
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left,self.top,self.width,self.height)
+
+
         self.statusBar().showMessage(self.statusMessage)
         self.show()
 
+
 if __name__=='__main__':
     app = QApplication(sys.argv)
+    path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'ICON.png')
+    app.setWindowIcon(QIcon(path))
     ex = App() #calls App class
     sys.exit(app.exec_())
