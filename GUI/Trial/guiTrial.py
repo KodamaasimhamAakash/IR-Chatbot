7c31dc02995e194fbc2a728f6ad52a49cdbc6f05
ShakthiSampath <shakthivvbhss@gmail.com>
2017-04-19 20:32:57 +0530

    Added ICON.png

--- GUI/Trial/guiTrial.py
+++ GUI/Trial/guiTrial.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QVBoxLayout,QPushButton,QMainWindow
 from PyQt5.QtGui import QIcon
 
@@ -17,10 +18,14 @@ class App(QMainWindow):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left,self.top,self.width,self.height)
+
+
         self.statusBar().showMessage(self.statusMessage)
         self.show()
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
+    path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'ICON.png')
+    app.setWindowIcon(QIcon(path))
     ex = App() #calls App class
     sys.exit(app.exec_())
