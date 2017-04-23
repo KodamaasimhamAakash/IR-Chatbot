598fe11a39c06cb978824f9e78e00fe7e5c2a86b
ShakthiSampath <shakthivvbhss@gmail.com>
2017-04-23 13:49:39 +0530

    Update GUI.py

--- GUI/Trial/GUI.py
+++ GUI/Trial/GUI.py
@@ -13,7 +13,7 @@ class App(QMainWindow):
         self.top = 200
         self.left = 550
         self.height = 700
-        self.width = 500
+        self.width = 520
         self.initUI()  # next method call
 
     def initUI(self):
@@ -23,8 +23,8 @@ class App(QMainWindow):
 
         # initializations
 
-        Message = QLineEdit(self)
-        Message.setFixedWidth(450)
+        self.Message = QLineEdit(self)
+        self.Message.setFixedWidth(450)
         '''Box = QLabel(self)                              # For Username
         Box.setStyleSheet('background-color: white;')
         Box.setFixedWidth(150)
@@ -41,35 +41,52 @@ class App(QMainWindow):
         #Enter = QLabel(self)
         #EnterLogo = QPixmap('EnterIcon.png')
         #Enter.setPixmap(EnterLogo)
-        enter = QPushButton('Enter',self)
-        enter.setToolTip('Press to Enter Message')
-        enter.setFixedWidth(100)
-        enter.setShortcut('Enter')
-        enter.clicked.connect(self.on_click)
+        
+        self.enter = QPushButton('Enter',self)
+        self.enter.setToolTip('Press to Enter Message')
+        self.enter.setFixedWidth(50)
+        self.enter.setShortcut('Enter')
+        self.enter.clicked.connect(self.on_click)
+        self.newMessageLabel = QLabel(self)
+        self.Message.returnPressed.connect(self.enter.click)
+        self.enter.setAutoDefault(True)
+
+
         h_box_Message = QHBoxLayout()
         h_box_User = QHBoxLayout()
-        h_box_Message.addWidget(Message)
-        h_box_Message.addWidget(enter)
+        h_box_Message.addWidget(self.Message)
+        h_box_Message.addWidget(self.enter)
         h_box_Message.addStretch()
         #h_box_User.addWidget(Username)
         #h_box_User.addWidget(User)
         h_box_User.addStretch()
+        self.yaxis = 600
+        self.count = 0
+        self.
+        self.Message.move(10,650)
+        self.enter.move(465,650)
 
-        Message.move(10,650)
-        enter.move(465,650)
+
+
         #Username.move(370,10)
         #User.move(450,10)
         #Box.move(350,5)
 
         #self.fileUserPic()
         #self.messageUser()
-        self.show()
+
 
     @pyqtSlot()
     def on_click(self):
-        
-
-
+        self.count + 1
+        for
+        self.newMessageLabel.setFixedWidth(450)
+        self.newMessageLabel.move(10,self.yaxis)
+        self.newMessageLabel.setStyleSheet('background-color:white')
+        messageEntered = self.Message.text()
+        self.newMessageLabel.setText(messageEntered);
+        self.Message.clear()
+        self.yaxis = self.yaxis - 50
 
 
 if __name__=='__main__':
@@ -77,4 +94,5 @@ if __name__=='__main__':
     path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'ICON.png')
     app.setWindowIcon(QIcon(path))
     ex = App() #calls App class
+    ex.show()
     sys.exit(app.exec_())
